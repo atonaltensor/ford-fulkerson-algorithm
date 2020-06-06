9e885c6311c5f86706b3f8b067b3cfb892ccfeaf
atonaltensor <maxx@maxxcho.com>
2020-06-06 18:36:41 -0400

    Added path slicing methods and reimplemented Network class.
    Added slicing methods to Graph for tracing through paths.
    Reimplemented Network, using two separate instances of Graph as routes
    and flow.

--- objects.py
+++ objects.py
@@ -4,13 +4,19 @@ class Graph():
 		self.data = matrix
 	
 	#use slices to get weights along a path:	
-	def __getitem__(self, *path):
+	def __getitem__(self, path):
 		i = 0
 		result = []
 		while i+1 < len(path):
 			result.append(self.data[path[i]][path[i+1]])
 			i = i+1
 		return result
+		
+	def __setitem__(self, path, values):
+		i = 0
+		while i+1 < len(path):
+			self.data[path[i]][path[i+1]] = values[i]
+			i = i+1
 	
 	def link(self, from_node, to_node, weight = 1):
 		self.data[from_node][to_node] = weight
@@ -27,24 +33,27 @@ class Graph():
 		for each_row in self.data:
 			each_row.insert(loc,0)
 	
+	def get_dim(self):
+		return len(self.data)
 
-#Network is a subclass of Graph that also tracks flow.
-class Network(Graph):
+
+#Network tracks both routes and flow, each of which are instances of Graph.
+class Network():
 	def __init__(self, matrix, starts, ends):
-		super().__init__(matrix)
+		self.data = Graph(matrix)
 		self.starts = starts
 		self.ends = ends
 		self.augment()
 		
 		#initialize zero matrix for tracking flow through network:
-		self.flow = [[0 for _ in each_row] for each_row in self.data]
+		self.flow = Graph([[0 for _ in self.data.get_dim()] for _ in self.data.get_dim()])
 		
 	def augment(self):
 		self.start = 0
 		self.end = len(self.data)
 		
-		self.add_node(self.start)
-		self.add_node(self.end)
+		self.data.add_node(self.start)
+		self.data.add_node(self.end)
 		
 		#all node labels are now off by 1:
 		self.starts = self.starts + 1
@@ -52,47 +61,41 @@ class Network(Graph):
 		
 		starts_outflow = []
 		for each in self.starts:
-			m = max(self.get_outflow(each))
+			m = max(self.data.get_outflow(each))
 			starts_outflow.append(m)
 		m = max(starts_outflow)
 		
 		ends_inflow = []
 		for each in self.ends:
-			n = max(self.get_inflow(each))
+			n = max(self.data.get_inflow(each))
 			ends_inflow.append(n)
 		n = max(ends_inflow)
 		
 		for each in starts:
-			self.link(self.start,each,m)
+			self.data.link(self.start,each,m)
 			
 		for each in ends:
-			self.link(each,self.end,n)
-			
-	def get_residual_flow(self):
-		#self.data - self.flow
-		neg_flow = [[-x for x in each] for each in self.flow]
-		#matrix addition:
-		return [[sum(i) for i in zip(each[0], each[1])] for each in zip(self.data, neg_flow)]
-		
-	def get_residual_flow_thru(self, path):
-		#self.data[path] - self.flow[path]
-		neg_flow = [[-x for x in each] for each in self.flow]
-		i = 0
-		result = []
-		while i+1 < len(path):
-			
-			i = i+1
-			
-	
-	def update_flow(self, path, water):
-		#flow water down the path (path is list of nodes)
-		i = 0
-		while i+1 < len(path):
-			self.flow[path[i]][path[i+1]] += water
-			i = i+1
-		
+			self.data.link(each,self.end,n)
+
 	def flow_thru_path(self, path):
-		f = min(self.get_residual_flow_thru(path))
+		f = min(self.get_residual_flow(path))
 		self.update_flow(path, f)
+
+	# def get_residual_flow(self):
+	# 	#self.data - self.flow
+	# 	neg_flow = [[-x for x in each] for each in self.flow]
+	# 	#matrix addition:
+	# 	return [[sum(i) for i in zip(each[0], each[1])] for each in zip(self.data, neg_flow)]
+
+	def get_residual_flow(self, path):
+		#self.data[path] - self.flow[path]
+		return [x-y for (x,y) in zip(self.data[path], self.flow[path])]
+
+	def update_flow(self, path, water):
+		#flow water down the path
+		#self.flow[path] += self.flow[path] + water
+		stream = [water] * len(path)
+		self.flow[path] = [x+y for (x,y) in zip(self.flow[path], stream)]
+		
 		
 		
